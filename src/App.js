777e2432abee5439b82100d6842effb456a6f868
Anjali <anjali.ghanshani@gmail.com>
2022-02-24 21:17:31 +1100

    firebase authentication implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
+  const user = null;
+
+  useEffect(()  => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+
+      }
+      
+    })
+
+  }, []);
   return (
     <div className="app">
-    <HomeScreen />
+    <Router>
+      {!user ? ( 
+      <LoginScreen />
+      ): (
+        <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/test" element={<h1>WOW WHATS UP</h1>} />
+      </Routes>
+      )}
+    </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp  } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCq4P8ihKaR3HcM0I4DJDX2jbOGbPz1D4k",
     authDomain: "netflix-clone-demo-69363.firebaseapp.com",
@@ -6,3 +10,16 @@ const firebaseConfig = {
     messagingSenderId: "636852356591",
     appId: "1:636852356591:web:89b3354ac174bef354221a"
   };
+
+  // const firebaseApp = Firebase.initializeApp(firebaseConfig);
+  // const db = firebaseApp.firestore();
+  // const auth = Firebase.auth();
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth();
+  //const getUserCred = createUserWithEmailAndPassword();
+
+
+  export { auth, db }
+ 
